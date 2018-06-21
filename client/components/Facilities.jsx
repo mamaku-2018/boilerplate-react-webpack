165383e318b666df622bb0280a02048a52ac6e5b
Caitlyn Bayley <caitlyn.bayley@gmail.com>
2018-06-21 19:49:05 +1200

    image tidy

--- client/components/Facilities.jsx
+++ client/components/Facilities.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import {HashRouter as Router, Route, Link} from 'react-router-dom'
 
-const Facilities = () => {
+import Sandwich from '../../images/facilities-sandwhich'
+
+const Facilities = (props) => {
   return (
     <div>
-      Fac
+      <div>
+        <h2>Facilities</h2>
+      </div>
+      <div className ='sandwich'>
+        <Sandwich image={props.image} alt={props.name} />
+      </div>
     </div>
   )
 }
