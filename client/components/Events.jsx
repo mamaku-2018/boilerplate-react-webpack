2a1f9286d01fb52e184e6345232e44ba0b8175a1
sungjin-shane <sjpark731015@gmail.com>
2018-06-22 15:53:54 +1200

    finished everything

--- client/components/Events.jsx
+++ client/components/Events.jsx
@@ -4,21 +4,19 @@ import {HashRouter as Router, Route, Link} from 'react-router-dom'
 const Events = () => {
   return (
     <div className='event-home'>
-      <script src="art-project.js"></script>
-      <script src="sound.js"></script>
-
       <div id='title'>
-        <h1 id='event-title'>Event Page</h1>
+        <h1 id='event-title'>Event in Cafekihini</h1>
         <img id='event-thought' src='images/event.jpg' alt='thought bubble'/>
       </div>
       <div>
-        <h1 id='event-contents'> This is the first event in DEV Academy </h1>
+        <h1 id='event-contents'> This is the first cultural event in Cafekihini </h1>
       </div>
       <div>
-        <h1 id='event-contents'> This is the firdsdsddst event in DEV Academy </h1>
+        <h1 id='event-contents'> 21:00 28/06/2018 </h1>
       </div>
-      <img classNamen ='event-submarine' src='images/submarine.png' id="submarine"/>
-      <img classNamen ='event-missile' src="images/missile3.png" width="0" id="seaMissile" />
+      <div className="eventvid">
+        <iframe id="event-video" width="454" height="280" src="https://www.youtube.com/embed/Cof2njz5BBw" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+      </div>
     </div>
   )
 }
