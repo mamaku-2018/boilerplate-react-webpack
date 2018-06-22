cdf692ca386ae1c476949b5ffe703e1b1c365425
bronburgundy <bron.cotter@icloud.com>
2018-06-22 14:43:25 +1200

    Added eventhandler for meow

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
 // import {HashRouter as Router, Route, Link} from 'react-router-dom'
 
-const Home = () => {
-  return (
-    <div className='content-home'>
-      <div id='title'>
-        <img id='thought' src='images/thought.png' alt='thought bubble'/>
-        <h1 id='main-title'>Cafe Kihini</h1>
+class Home extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      meow: new Audio('../public/audio/Cat_Meow.mp3')
+    }
+    this.playSound = this.playSound.bind(this)
+  }
+  playSound () {
+    this.state.meow.play()
+  }
+
+  render () {
+    return (
+      <div className='content-home'>
+        <div id='title'>
+          <img id='thought' onMouseOver={this.playSound} src='images/thought.png' alt='thought bubble'/>
+          <h1 id='main-title'>Cafe Kihini</h1>
+        </div>
+        <img id='toast' src='images/toaster.png' alt='toaster'/>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Home
