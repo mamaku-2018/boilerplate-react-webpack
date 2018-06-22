f0e90f4d63000cb10690eb85097b276ead9eb93b
sungjin-shane <sjpark731015@gmail.com>
2018-06-22 14:17:57 +1200

    add events page

--- client/components/Events.jsx
+++ client/components/Events.jsx
@@ -4,13 +4,21 @@ import {HashRouter as Router, Route, Link} from 'react-router-dom'
 const Events = () => {
   return (
     <div className='event-home'>
+      <script src="art-project.js"></script>
+      <script src="sound.js"></script>
+
       <div id='title'>
         <h1 id='event-title'>Event Page</h1>
         <img id='event-thought' src='images/event.jpg' alt='thought bubble'/>
       </div>
       <div>
-        <h1 id='event-title'>Cultural event</h1>
+        <h1 id='event-contents'> This is the first event in DEV Academy </h1>
       </div>
+      <div>
+        <h1 id='event-contents'> This is the firdsdsddst event in DEV Academy </h1>
+      </div>
+      <img classNamen ='event-submarine' src='images/submarine.png' id="submarine"/>
+      <img classNamen ='event-missile' src="images/missile3.png" width="0" id="seaMissile" />
     </div>
   )
 }
